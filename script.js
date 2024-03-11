ca6ec40400cd6e3f27542296152cd16da2e47268
Tarraka-Tech <renangarciagomes@gmail.com>
2024-03-11 11:20:44 -0300

    Tarefa concluída
    Tarefa 1 (Palíndromo) e Tarefa 2 (Mínimo e Máximo) concluídas.

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(frase) {
+    let fraseLimpa = '';
+    for (let i = 0; i < frase.length; i++) {
+        if (frase[i] !== ' ') {
+            fraseLimpa += frase[i].toLowerCase();
+        }
+    }
+
+    const fraseInvertida = fraseLimpa.split('').reverse().join('');
+
+    return fraseLimpa === fraseInvertida;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+    if (arr.length === 0) {
+        return [];
+    }
+
+    let minValue = arr[0];
+    let maxValue = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < minValue) {
+            minValue = arr[i];
+        }
+        if (arr[i] > maxValue) {
+            maxValue = arr[i];
+        }
+    }
+
+    return [minValue, maxValue];
 }
